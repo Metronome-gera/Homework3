171429fa8fb7f2929bf6b0494cfe5aaf0e7dfb39
Роман <roninmtn@gmail.com>
2025-07-21 13:10:39 +0400

    Почти готово  2

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,22 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
-        int gaba = 569 ;
-        byte dahun = 67 ;
-        short puer = 27897 ;
-        long tiguan = 987678965549L ;
-        float milkUlun = 2.786F ;
-        double ivan = 27.12 ;
-        long shuPuer = -159 ;
+// Задача №1
+        int gaba ;
+        byte dahun ;
+        short puer ;
+        long tiguan ;
+        float milkUlun ;
+        double ivan ;
+        long shuPuer ;
+// Задача №2
+        gaba = 569 ;
+        dahun = 67 ;
+        puer = 27897 ;
+        tiguan = 987678965549L ;
+        milkUlun = 2.786F ;
+        ivan = 27.12 ;
+        shuPuer = -159 ;
 
         System.out.println("Значение переменной " + " gaba " + " с типом " + " int " + " равно " + gaba);
         System.out.println("Значение переменной " + " dahun " + " с типом " + " byte " + " равно " + dahun);
@@ -18,14 +26,16 @@ public class Main {
         System.out.println("Значение переменной " + " milkUlun " + " с типом " + " flout " + " равно " + milkUlun);
         System.out.println("Значение переменной " + " ivan " + " с типом " + " double " + " равно " + ivan);
         System.out.println("Значение переменной " + " shuPuer " + " с типом " + " long " + " равно " + shuPuer);
-
+// Задача №3
         byte ludPavlova = 23;
         byte annaSergeevna = 27;
         byte katerinaAndreevna = 30;
         short paper = 480;
         long paperStudent = paper / (ludPavlova + annaSergeevna + katerinaAndreevna);
         System.out.println("На каждого ученика расчитано "+paperStudent+" листков бумаги");
+// Задача №4
 
+// Задача №5
         byte krask = 120;
         byte kraskBelKab = 2;
         byte kraskKorKab = 4;
@@ -66,6 +76,22 @@ public class Main {
 
          byte averageDay = (byte) ((minDay+maxDay)/2);
          System.out.println("Среднее время похудения спортсмена "+averageDay+" день");
+
+         int zpMasha = 67760;
+         int zpDenis = 83690;
+         int zpKris = 76230;
+         float zpIncreasProcent = 0.10F;
+         int zpMashaIncrease = (int) ((zpMasha*zpIncreasProcent)+zpMasha);
+         System.out.println(zpMashaIncrease);
+         int zpDenisIncrease = (int) ((zpDenis*zpIncreasProcent)+zpDenis);
+         int zpKrisIncrease = (int) ((zpKris*zpIncreasProcent)+zpKris);
+         int mashaDifference = (zpMashaIncrease-zpMasha);
+         int denisDifference = (zpDenisIncrease-zpDenis);
+         int krisDifference = (zpKrisIncrease-zpKris);
+
+         System.out.println("После повышения Маша станет получать "+zpMashaIncrease+" рублей. Этона на "+mashaDifference+" рублей больше чем до этого");
+        System.out.println("После повышения Денис станет получать "+zpDenisIncrease+" рублей. Этона на "+denisDifference+" рублей больше чем до этого");
+        System.out.println("После повышения Кристина станет получать "+zpKrisIncrease+" рублей. Этона на "+krisDifference+" рублей больше чем до этого");
 
 
     }
